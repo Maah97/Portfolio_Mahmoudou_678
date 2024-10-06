54e03870f7987a118656013673dbb7804fbaa0f4
Maah97 <mahmoudouaboul@gmail.com>
2024-10-06 17:12:18 +0100

    Organisation of the project with the creating of pages files and components of header and footer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Projet from './pages/projet';
+import Error from './pages/error'
+import Header from "./composants/Header";
+import Footer from "./composants/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projet/:id" element={<Projet />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   </React.StrictMode>
 );
