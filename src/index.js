98764a3b396fbf71325135421988533e517cd5d9
Maah97 <mahmoudouaboul@gmail.com>
2024-10-19 12:44:31 +0100

    basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './styles_SCSS/app.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="https://Maah97.github.io/portfolio_mahmoudou">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
